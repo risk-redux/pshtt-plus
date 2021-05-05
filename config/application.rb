756f27dc24b0afb71d45c5f0fb6a024fa4b4f681
Karim Said <karim.a.said@nasa.gov>
2021-05-04 22:24:22 -0400

    Trying out Observables embedding.

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module PshttPlus
 
     # To support CORS for Observable requests.
     config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin' => 'https://risk-redux-pshtt-plus.herokuapp.com',
+      'Access-Control-Allow-Origin' => '*',
       'Access-Control-Request-Method' => "GET"
     }
   end
